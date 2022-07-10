c2bf28d75fa77750be84daab699b3609aa5fa25d
salman11o <52657598+salman11o@users.noreply.github.com>
2022-07-11 00:17:11 +0500

    Add files via upload

---
+++ work/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { CoursesComponent } from './courses/courses.component';
+import { CoursesService } from './courses/courses.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CoursesComponent,
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [CoursesService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ work/src/app/courses/courses.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CoursesService {
+
+  constructor() { }
+  getc()
+  {
+    return ["course 1", "course 2", "course 3", "course 4"];
+  }
+}
